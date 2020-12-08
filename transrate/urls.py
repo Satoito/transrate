c45bf53caee7b65d0580d831cadd8f77d7a3fc8c
unknown <kwge.blue.hunter.5@gmail.com>
2020-12-08 14:40:09 +0900

    add file_upload

--- transrate/urls.py
+++ transrate/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+import app.views as file_upload
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('app/', include('app.urls')),
+    path('success/url/', file_upload.success),
 ]
